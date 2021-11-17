9c879d6776ce99eac25268e9ac13619bb1f86afd
benpaquier <hello@benpaquier.com>
2021-11-17 15:17:24 +0100

    Formik

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
-function App() {
+const App = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "benoit",
+      password: ""
+    },
+    onSubmit: values => {
+      console.log(values)
+    },
+    validationSchema: Yup.object().shape({
+      username: Yup.string()
+        .min(5, "Username est trop court")
+        .required("Username est requis"),
+      password: Yup.string()
+        .min(3, "password trop court")
+        .max(10, "password trop long")
+    }),
+    // validateOnChange: false
+  })
+
+  console.log(formik.errors)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={formik.handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        placeholder="enter username"
+        value={formik.values.username}
+        onChange={formik.handleChange}
+      />
+      {formik.errors.username && <p>{formik.errors.username}</p>}
+      <br />
+      <input
+        type="password"
+        name="password"
+        placeholder="enter password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+      />
+      {formik.errors.password && <p>{formik.errors.password}</p>}
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  )
 }
 
-export default App;
+export default App
